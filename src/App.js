eb9df9949c727e2230b5f1adc5b356982c7b7d26
ducbv <ducbv@geniee.co.jp>
2022-04-14 18:24:49 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from './components/Todo';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Tools</h1>
+      <Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
