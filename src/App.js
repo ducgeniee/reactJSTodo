21aff8bbd6f244e3b638a7bf37657d17925e94b5
ducbv <ducbv@geniee.co.jp>
2022-04-19 10:37:04 +0700

    Add state and props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Todo from './components/Todo';
+
 import './App.css';
 
 function App() {
-  return (
+  return ( 
     <div>
       <h1>My Tools</h1>
-      <Todo></Todo>
+      <Todo text='somthing01' />
+      <Todo text='somthing02' />
+      <Todo text='somthing03' />
     </div>
   );
 }
